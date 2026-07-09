f4645a336d2dee0b49ad7a42c702c8212760c95c
Rajeswari M <mrajeswariraji66@gmail.com>
2026-07-09 21:27:21 +0530

    Create User_activity_for_the_past_days.sql

---
+++ User_activity_for_the_past_days.sql
@@ -0,0 +1,1 @@
+SELECT activity_date AS day, COUNT(*) AS active_users FROM (SELECT DISTINCT user_id, activity_date FROM Activity WHERE activity_date >= '2019-06-28' AND activity_date <= '2019-07-27')GROUP BY activity_date;
